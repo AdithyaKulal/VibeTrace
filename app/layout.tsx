9fe03ce4c9f84ee7636950a9bf0fd5cefe8841b7
AdithyaKulal <adithyakulal94@gmail.com>
2026-05-07 00:05:39 +0530

    LandingPage UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+/*This layout whatever the changes u will make it applicable to entire layout */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
-
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,14 +31,22 @@ const session= await auth()
 
   return (
     <SessionProvider session={session}>
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-        <body className="min-h-full flex flex-col">{children}
-          
-      </body>
-    </html>
+      <html
+        lang="en"
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </SessionProvider>
   );
 }
